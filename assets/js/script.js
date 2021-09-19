eb6e91ae3b152f154aa1408036f2240483c271e9
sehaj97 <sehajmagan@gmail.com>
2021-09-19 18:26:30 -0400

    added quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,38 +22,122 @@ var penaltyTime = 0;
 var quizData = [
     {
         "id": 1,
-        "question": "Question1?",
+        "question": "What is right way to Add an item to the end of an Array named fruits?",
         "answers": {
-          "answer_a": "wrong",
-          "answer_b": "correct",
-          "answer_c": "wrong",
-          "answer_d": "wrong"
+          "answer_a": "fruits.onArray('Orange')",
+          "answer_b": "fruits.push('Orange')",
+          "answer_c": "fruits.insideArray('Orange')",
+          "answer_d": "fruits.pop('Orange')"
         },
         "correct_answer_id": "answer_b"
       },
       
       {
         "id": 2,
-        "question": "Question2?",
+        "question": "______ method removes the first element from an array and returns that removed element.",
         "answers": {
-          "answer_a": "correct",
-          "answer_b": "wrong",
-          "answer_c": "wrong",
-          "answer_d": "wrong"
+          "answer_a": "slice",
+          "answer_b": "unshift",
+          "answer_c": "shift",
+          "answer_d": "pop"
+        },
+        "correct_answer_id": "answer_c"
+      },
+      
+      {
+        "id": 3,
+        "question": "object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.",
+        "answers": {
+          "answer_a": "promise",
+          "answer_b": "async function",
+          "answer_c": "generator",
+          "answer_d": "proxy"
         },
         "correct_answer_id": "answer_a"
       },
       
       {
-        "id": 3,
-        "question": "Question3?",
+        "id": 4,
+        "question": "____________ object enables language-sensitive date and time formatting.",
         "answers": {
-          "answer_a": "wrong",
-          "answer_b": "wrong",
-          "answer_c": "correct",
-          "answer_d": "wrong"
+          "answer_a": "Intl.DateTimeFormat",
+          "answer_b": "Intl.PluralRules",
+          "answer_c": "Intl.NumberFormat",
+          "answer_d": "Intl.Locale"
+        },
+        "correct_answer_id": "answer_a"
+      },
+      
+      {
+        "id": 5,
+        "question": "which of the following are methods of array?",
+        "answers": {
+          "answer_a": "Array.prototype.entries()",
+          "answer_b": "Array.prototype.flat()",
+          "answer_c": "Array.prototype.find()",
+          "answer_d": "All of the Above"
+        },
+        "correct_answer_id": "answer_d"
+      },
+      
+      {
+        "id": 6,
+        "question": "What is use of String.prototype.charAt()?",
+        "answers": {
+          "answer_a": "returns a string created from the specified sequence of UTF-16 code units.",
+          "answer_b": "returns a new string consisting of the single UTF-16 code unit located at the specified offset into the string.",
+          "answer_c": "returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.",
+          "answer_d": "returns a non-negative integer that is the UTF-16 code point value."
+        },
+        "correct_answer_id": "answer_b"
+      },
+      
+      {
+        "id": 7,
+        "question": "_________ property returns a live HTMLCollection which contains all of the child elements of the document upon which it was called.",
+        "answers": {
+          "answer_a": "children",
+          "answer_b": "append",
+          "answer_c": "parent",
+          "answer_d": "kidNodes"
+        },
+        "correct_answer_id": "answer_a"
+      },
+      
+      {
+        "id": 8,
+        "question": "you can get or set the current title of the document using _______",
+        "answers": {
+          "answer_a": "var docTitle = document.contentTitle;",
+          "answer_b": "var docTitle = document.titleName;",
+          "answer_c": "var docTitle = document.title;",
+          "answer_d": "var docTitle = document.heading;"
         },
         "correct_answer_id": "answer_c"
+      },
+      
+      {
+        "id": 9,
+        "question": "object holds key-value pairs and remembers the original insertion order of the keys?",
+        "answers": {
+          "answer_a": "map",
+          "answer_b": "string",
+          "answer_c": "int",
+          "answer_d": "local storage"
+        },
+        "correct_answer_id": "answer_a"
+      },
+      
+      {
+        "id": 10,
+        "question": "method _________ sets up a function that will be called whenever the specified event is delivered to the target.",
+        "answers": {
+          "answer_a": "addeventlistener()",
+          "answer_b": "addEvent()",
+          "answer_c": "clickTarget()",
+          "answer_d": "addEventListener()"
+        },
+        "correct_answer_id": "answer_d"
       }
   ]
 
